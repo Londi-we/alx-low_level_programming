7ab30908a2642800b270d6b5b96c83cbdf3aaf17
Londi-we <ndiwentsha@gmail.com>
2023-03-24 19:05:47 -0700

    task0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * positive_or_negative - print positive or negative digit
+ *
+ * @i: number seen onoutput
+ *
+ * Return: 0 for now
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i)
+	else
+		printf("%d is positive\n", i)
+}
